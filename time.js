03bf8aba74284f37547401e54e904ee4ae3a308c
Tzuyi Yang <a0988426059@gmail.com>
2019-05-18 18:38:07 +0800

    middlewareII

--- time.js
+++ time.js
@@ -1,7 +1,11 @@
 function time(req, res, next) {
-  const nowDate = new Date()
-  console.log(nowDate.toLocaleString() + ' | ' + req.method + ' from ' + req.url);
-  next();
+  const start = new Date()
+  console.log(start.toLocaleString() + ' | ' + req.method + ' from ' + req.url)
+
+  const end = new Date().valueOf()
+  const timeout = end - start.valueOf()
+  console.log(start.toLocaleString() + ' | ' + req.method + ' from ' + req.url + ' | total time: ' + timeout + 'ms')
+  next()
 }
 
 module.exports = time
