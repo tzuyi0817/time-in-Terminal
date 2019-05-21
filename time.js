5bb546cf7ae4bbb60dd1ede7a7903378e619d759
Tzuyi Yang <a0988426059@gmail.com>
2019-05-21 21:30:38 +0800

    change widdlewareII

--- time.js
+++ time.js
@@ -1,10 +1,13 @@
 function time(req, res, next) {
   const start = new Date()
-  console.log(start.toLocaleString() + ' | ' + req.method + ' from ' + req.url)
 
-  const end = new Date().valueOf()
-  const timeout = end - start.valueOf()
-  console.log(start.toLocaleString() + ' | ' + req.method + ' from ' + req.url + ' | total time: ' + timeout + 'ms')
+  if (req.url !== '/favicon.ico') {
+    res.on('finish', () => {
+      const end = new Date().valueOf()
+      const timeout = end - start.valueOf()
+      console.log(start.toLocaleString() + ' | ' + req.method + ' from ' + req.url + ' | total time: ' + timeout + 'ms')
+    })
+  }
   next()
 }
 
